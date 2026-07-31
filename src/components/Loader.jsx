8c4dcb7a49ac14e970b5611fbc333c022e00b9b4
stefinmathew_ <stefinmathew66@gmail.com>
2026-07-31 22:56:39 +0530

    Apply custom Velnora logo style with swash 'l' on loading screen

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -29,8 +29,19 @@ export default function Loader({ onFinish }) {
 
   return (
     <div className={`loader-wrapper ${fadeOut ? 'fade-out' : ''}`}>
-      <div className="loader-brand">
-        V E L N O R A
+      <div className="loader-brand velnora-logo">
+        <span className="logo-letter logo-letter-v">v</span>
+        <span className="logo-letter logo-letter-e">e</span>
+        <span className="logo-l-wrapper">
+          <span className="logo-l-spacer">l</span>
+          <svg className="logo-l-svg" viewBox="0 0 120 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 97 10 H 105 V 13 H 102 V 50 C 102 58, 85 68, 50 68 C 25 68, 8 62, 2 54 C 0 51.5, 1 50, 3 50 C 5 50, 10 56, 25 61 C 45 66, 75 64, 98 51 V 13 H 97 Z" fill="currentColor" />
+          </svg>
+        </span>
+        <span className="logo-letter">n</span>
+        <span className="logo-letter">o</span>
+        <span className="logo-letter">r</span>
+        <span className="logo-letter">a</span>
       </div>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <div className="loader-bar-bg">
